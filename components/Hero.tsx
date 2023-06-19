3ac42c6d60875577b79c6ffe0392bd2b2b2658cd
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-06-20 02:16:28 +0600

    header and footer commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,9 +23,9 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-          <div className="hero__image-overlay"/>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
