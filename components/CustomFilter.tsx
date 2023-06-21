9aa8e4ab3259ce726cfa5482165e52d603d20b1a
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-06-22 02:13:06 +0600

    customFilter bug update

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Fragment, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 import { updateSearchParams } from "@/utils";
