1d3cd845d64dd5e76f2235fdea9b0a2cae4a3c40
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-06-22 01:27:44 +0600

    search functionalities and server side rendering commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -14,13 +20,16 @@ export default async function Home() {
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
+
         <div className="home__filters">
           <SearchBar />
+
           <div className="home__filter-container">
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
           </div>
         </div>
+
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,16 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters:FilterProps) {
+
+  const { manufacturer, year, fuel, limit, model } = filters;
+
   const headers = {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
